03e60aa2f3ef83573747b90d0adc34d2c68f5394
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-04-21 22:02:50 +0300

    Pass LanguageSettingsProvider to AnalyzerFacade explicitly
    This is helpful to invoke AnalyzerFacade.setupResolverForProject in
    tests where configuring the service implementation is tricky (it's
    usually done in production code in KotlinCoreEnvironment)

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/resolve/ModuleDependencyMapper.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/resolve/ModuleDependencyMapper.kt
@@ -16,6 +16,7 @@
 
 package org.jetbrains.kotlin.idea.caches.resolve
 
+import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.ProjectJdkTable
@@ -23,10 +24,7 @@ import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.roots.JdkOrderEntry
 import com.intellij.openapi.roots.LibraryOrderEntry
 import com.intellij.openapi.roots.ModuleRootManager
-import org.jetbrains.kotlin.analyzer.AnalyzerFacade
-import org.jetbrains.kotlin.analyzer.ModuleContent
-import org.jetbrains.kotlin.analyzer.ModuleInfo
-import org.jetbrains.kotlin.analyzer.ResolverForProject
+import org.jetbrains.kotlin.analyzer.*
 import org.jetbrains.kotlin.builtins.KotlinBuiltIns
 import org.jetbrains.kotlin.context.GlobalContextImpl
 import org.jetbrains.kotlin.context.withProject
@@ -81,7 +79,9 @@ fun createModuleResolverProvider(
         return AnalyzerFacade.setupResolverForProject(
                 debugName, globalContext.withProject(project), modulesToCreateResolversFor,
                 { module -> AnalyzerFacadeProvider.getAnalyzerFacade(module.platform ?: platform) },
-                modulesContent, jvmPlatformParameters, IdeaEnvironment, builtInsProvider,
+                modulesContent, jvmPlatformParameters,
+                ServiceManager.getService(LanguageSettingsProvider::class.java),
+                IdeaEnvironment, builtInsProvider,
                 delegateResolver, { _, c -> IDEPackagePartProvider(c.moduleContentScope) },
                 sdk?.let { SdkInfo(project, it) },
                 modulePlatforms = { module -> module.platform?.multiTargetPlatform }
